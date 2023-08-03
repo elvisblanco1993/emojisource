576320c0fb6b7e5303b6b2128622b7802b9e4bc7
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2023-08-03 14:36:10 -0400

    ..

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,18 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
+
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SJ3L10RKGY"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-SJ3L10RKGY');
+        </script>
+
     </head>
     <body class="bg-slate-900 text-white">
         <div class="font-sans antialiased">
